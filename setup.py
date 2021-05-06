1f70086071c5855995e19d1e033461327e071348
FontToolcSLI <fonttoolscli@protonmail.com>
2021-05-06 07:37:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='ftcli',
-    version='0.1.0',
+    version='0.1.1',
     description='ftCLI',
     packages=setuptools.find_packages(),
     entry_points = {
