7c5ea85e14b73e6afc18ca59f48170366c659cf4
ftCLI <fonttoolscli@protonmail.com>
2022-01-15 12:05:08 +0100

    Version 0.6.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='ftcli',
-    version='0.5.7',
+    version='0.6.0',
     description='ftCLI',
     packages=setuptools.find_packages(),
     include_package_data=True,
