133ebd87dc455858f4a04f861258d2211eab20b6
ftCLI <ftCLI@proton.me>
2023-09-16 08:13:45 +0200

    [Lib.utils.cli_tools]: added the 'recursive' param

--- foundryToolsCLI/Lib/utils/cli_tools.py
+++ foundryToolsCLI/Lib/utils/cli_tools.py
@@ -7,12 +7,42 @@ from foundryToolsCLI.Lib.Font import Font
 from foundryToolsCLI.Lib.VFont import VariableFont
 
 
-def get_variable_fonts_in_path(input_path: pathlib.Path, recalc_timestamp: bool = False) -> list[VariableFont]:
+def get_files_in_path(input_path: pathlib.Path, recursive: bool = False) -> list[pathlib.Path]:
+    """
+    Get a list of files from a path. If the path is a directory, the function will return a list of all files found in
+    the directory. If ``recursive`` is True, the function will recursively search for files in subdirectories. If the
+    path is a file, the function will return a list containing only one file.
+    :param input_path: path to the input file or directory
+    :param recursive: If True, the function will recursively search for files in subdirectories
+    :return: A list of pathlib.Path objects representing the files found in the path
+    """
     files = []
     if input_path.is_file():
         files.append(input_path.resolve())
     if input_path.is_dir():
-        files = [p.resolve() for p in input_path.iterdir() if p.is_file()]
+        if recursive:
+            files = [p.resolve() for p in input_path.rglob("*") if p.is_file()]
+        else:
+            files = [p.resolve() for p in input_path.iterdir() if p.is_file()]
+
+    return files
+
+
+def get_variable_fonts_in_path(
+    input_path: pathlib.Path, recursive: bool = False, recalc_timestamp: bool = False
+) -> list[VariableFont]:
+    """
+    Get a list of VariableFont objects from a path. If the path is a directory, the function will return a list of all
+    VariableFont objects found in the directory. If ``recursive`` is True, the function will recursively search for
+    VariableFonts in subdirectories. If the path is a file, the function will return a list containing only one
+    VariableFont.
+    :param input_path: path to the input file or directory
+    :param recursive: If True, the function will recursively search for VariableFonts in subdirectories
+    :param recalc_timestamp: If True, the function ``modified`` timestamp in the ``head`` table will be recalculated
+    on save
+    :return: A list of VariableFont objects
+    """
+    files = get_files_in_path(input_path, recursive=recursive)
 
     variable_fonts = []
     for file in files:
@@ -27,6 +57,7 @@ def get_variable_fonts_in_path(input_path: pathlib.Path, recalc_timestamp: bool 
 
 def get_fonts_in_path(
     input_path: pathlib.Path,
+    recursive: bool = False,
     recalc_timestamp: bool = False,
     allow_extensions: list = None,
     allow_ttf=True,
@@ -36,11 +67,25 @@ def get_fonts_in_path(
     allow_web_font=True,
     allow_sfnt=True,
 ) -> list[Font]:
-    files = []
-    if input_path.is_file():
-        files.append(input_path.resolve())
-    if input_path.is_dir():
-        files = [p.resolve() for p in input_path.iterdir() if p.is_file()]
+    """
+    Get a list of Font objects from a path, optionally filtering by extension and font type. If the path is a directory,
+    the function will return a list of all Font objects found in the directory. If ``recursive`` is True, the function
+    will recursively search for fonts in subdirectories. If the path is a file, the function will return a list
+    containing only one Font.
+
+    :param input_path: The path to the input file or directory
+    :param recursive: If True, the function will recursively search for fonts in subdirectories
+    :param recalc_timestamp: If True, the ``modified`` timestamp in the ``head`` table will be recalculated on save
+    :param allow_extensions: A list of allowed extensions. If specified, only fonts with extensions in the list will be returned.
+    :param allow_ttf:   If False, TrueType fonts will be excluded from the list
+    :param allow_cff:  If False, CFF fonts will be excluded from the list
+    :param allow_static: If False, static fonts will be excluded from the list
+    :param allow_variable: If False, variable fonts will be excluded from the list
+    :param allow_web_font: If False, web fonts will be excluded from the list
+    :param allow_sfnt: If False, sfnt fonts will be excluded from the list
+    :return: A list of Font objects
+    """
+    files = get_files_in_path(input_path, recursive=recursive)
 
     fonts = []
     for file in files:
@@ -93,7 +138,6 @@ def get_output_dir(input_path: pathlib.Path, output_dir: pathlib.Path = None) ->
 def initial_check_pass(fonts: list, output_dir: pathlib.Path) -> bool:
     """
     Checks if the list of fonts is not empty and if the output directory is writable.
-
     :param fonts: a list Font objects
     :param output_dir: the output directory
     :return: False if one of the checks fails, True if both checks succeed
@@ -110,6 +154,11 @@ def initial_check_pass(fonts: list, output_dir: pathlib.Path) -> bool:
 
 
 def get_project_files_path(input_path: pathlib.Path) -> pathlib.Path:
+    """
+    Get the path to the directory containing the project files (styles_mapping.json, fonts_data.csv).
+    :param input_path:
+    :return:
+    """
     if input_path.is_file():
         project_files_path = input_path.parent.joinpath("ftCLI_files")
     else:
@@ -119,6 +168,11 @@ def get_project_files_path(input_path: pathlib.Path) -> pathlib.Path:
 
 
 def get_style_mapping_path(input_path: pathlib.Path) -> pathlib.Path:
+    """
+    Get the path to the styles_mapping.json file.
+    :param input_path: Path to the input file or directory
+    :return: A pathlib.Path object representing the styles_mapping.json file
+    """
     project_files_dir = get_project_files_path(input_path)
     styles_mapping_file = pathlib.Path.joinpath(project_files_dir, "styles_mapping.json")
 
@@ -126,6 +180,11 @@ def get_style_mapping_path(input_path: pathlib.Path) -> pathlib.Path:
 
 
 def get_fonts_data_path(input_path: pathlib.Path) -> pathlib.Path:
+    """
+    Get the path to the fonts_data.csv file.
+    :param input_path: Path to the input file or directory
+    :return: A pathlib.Path object representing the fonts_data.csv file
+    """
     project_files_dir = get_project_files_path(input_path)
     fonts_data_file = pathlib.Path.joinpath(project_files_dir, "fonts_data.csv")
 
