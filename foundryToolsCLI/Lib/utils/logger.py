9c39a00bba4ebf7af5176463fa5ffb6bf63746b2
ftCLI <ftCLI@proton.me>
2023-09-17 13:53:41 +0200

    Update logger.py

--- foundryToolsCLI/Lib/utils/logger.py
+++ foundryToolsCLI/Lib/utils/logger.py
@@ -1,5 +1,7 @@
 import sys
 
+from pathlib import Path
+
 from loguru import logger
 
 # Remove the default logger
@@ -9,10 +11,19 @@ logger.remove(0)
 logger.add(
     sys.stdout,
     level="INFO",
+    backtrace=False,
     colorize=True,
-    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
-           "<level>{level: <8}</level> | "
-           "<level>{message}</level>",
+    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | " "<level>{level: <8}</level> | " "<level>{message}</level>",
 )
 
-__all__ = ["logger"]
+
+class Logs:
+    file_saved = "File saved: {file}"
+    file_not_changed = "File not changed: {file}"
+    file_not_selected = "File not selected: {file}"
+    file_not_exists = "File does not exist: {file}"
+    no_valid_fonts = "No valid fonts found: {input_path}"
+    x_height_not_defined = "sxHeight is defined only in OS/2 version 2 and up. Current OS/2 version is {version}"
+
+
+__all__ = ["logger", "Logs"]
