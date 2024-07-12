b218c5f2bae67c3a55a1ab16ebd12f58123adfd0
ftCLI <ftCLI@proton.me>
2024-07-12 10:57:39 +0200

    Bump version: 1.1.14 → 1.1.15

--- foundryToolsCLI/__init__.py
+++ foundryToolsCLI/__init__.py
@@ -1,3 +1,3 @@
-VERSION = __version__ = "1.1.14"
+VERSION = __version__ = "1.1.15"
 
 __all__ = ["VERSION"]
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def _get_requirements():
 
 setuptools.setup(
     name="foundrytools-cli",
-    version="1.1.14",
+    version="1.1.15",
     description="A set of command line tools to inspect, manipulate and convert font files",
     long_description=long_description,
     long_description_content_type="text/markdown",
