d6c0dfb7c1300a4b8e1c890aefbc6d211ec64cb5
ftCLI <ftCLI@proton.me>
2024-12-04 08:21:12 +0100

    Bump version: 1.1.18 → 1.1.19

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def _get_requirements():
 
 setuptools.setup(
     name="foundrytools-cli",
-    version="1.1.18",
+    version="1.1.19",
     description="A set of command line tools to inspect, manipulate and convert font files",
     long_description=long_description,
     long_description_content_type="text/markdown",
