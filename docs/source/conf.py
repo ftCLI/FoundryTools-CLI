cf8d105ddccffe70268e873938666f3f7b32ba4b
Cesare Gilento <ftcli@proton.me>
2025-05-11 10:27:31 +0200

    Rename project from FoundryTools-CLI-NG to FoundryTools-CLI

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "FoundryTools-CLI-NG"
+project = "FoundryTools-CLI"
 copyright = "2025, Cesare Gilento"
 author = "Cesare Gilento"
 release = "2.0.0"
