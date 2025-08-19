7bf8ef7fe1a44e7dd41681ee4806a5eea696a006
Cesare Gilento <ftCLI@proton.me>
2025-08-19 17:18:39 +0200

    Run pre-commit

--- src/foundrytools_cli/commands/print/vf_instances.py
+++ src/foundrytools_cli/commands/print/vf_instances.py
@@ -1,5 +1,3 @@
-from shutil import get_terminal_size
-
 from fontTools.ttLib.tables._f_v_a_r import Axis, NamedInstance
 from foundrytools import Font
 from rich.console import Console
@@ -14,14 +12,13 @@ def main(font: Font) -> None:
     """
     Prints the variable font instances in a table format.
     """
-    terminal_width = min(TERMINAL_WIDTH, get_terminal_size()[0] - 1)
     console = Console()
 
     table = Table(
         title="\nftCLI - Variable Font Instances Viewer",
         show_header=True,
         header_style="bold cyan",
-        title_style="bold green"
+        title_style="bold green",
     )
 
     axes: list[Axis] = font.t_fvar.table.axes
@@ -35,12 +32,18 @@ def main(font: Font) -> None:
     table.add_column("subfamilyNameID")
 
     for count, instance in enumerate(instances, start=1):
-        subfamily_name = f"{instance.subfamilyNameID}: {font.t_name.table.getDebugName(instance.subfamilyNameID)}"
-        postscript_name = f"{instance.postscriptNameID}: {font.t_name.table.getDebugName(instance.postscriptNameID)}"
+        subfamily_name = (
+            f"{instance.subfamilyNameID}: "
+            f"{font.t_name.table.getDebugName(instance.subfamilyNameID)}"
+        )
+        postscript_name = (
+            f"{instance.postscriptNameID}: "
+            f"{font.t_name.table.getDebugName(instance.postscriptNameID)}"
+        )
         table.add_row(
             str(count),
             *[str(v) for k, v in instance.coordinates.items() if k in [a.axisTag for a in axes]],
             postscript_name,
-            subfamily_name
+            subfamily_name,
         )
     console.print(table)
