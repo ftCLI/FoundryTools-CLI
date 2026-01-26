c38bc0c5dff5af7e23174c0c080cd16f1e3914d8
Cesare Gilento <ftCLI@proton.me>
2026-01-26 08:07:24 +0100

    Bump version to 2.1.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "FoundryTools-CLI"
 copyright = "2025, Cesare Gilento"
 author = "Cesare Gilento"
-release = "2.1.0"
+release = "2.1.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
